057a0ae9780134df3e9508eb0d902ca3e14ba4df
Angels <anglsant2002@hotmail.com>
2025-01-25 17:55:05 -0300

    oneline usando tupla

--- test_python.py
+++ test_python.py
@@ -9,6 +9,13 @@ print(lista_estudiantes)
 def retornar_estudante():
     return 'Angel', 22, 7.5
 nome_estudante, idade_estudante, promedio_estudante =   retornar_estudante()
-print(nome_estudante)
-print(idade_estudante)
-print(promedio_estudante)
+print("O nome é:", nome_estudante)
+print("A idade é", idade_estudante)
+print("O promedio é:", promedio_estudante)
+
+#Onelineswapping
+variable_1 = 1.0
+variable_2 = 2.0
+#Para mudar o valor entre elas, pode ser usada uma tupla
+variable_1, variable_2 = variable_2, variable_1
+print(variable_1, variable_2)
